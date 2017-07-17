5d6eea3fe823e3273590cc05788cd0d5d747cae4
carlosmendeznlp <carlos.mendez.nlp@gmail.com>
2017-07-16 19:36:12 -0500

    Automatic summarization of TFs properties

--- automatic-summarization-TFs.sh
+++ automatic-summarization-TFs.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-./NLP-preprocessing-pipeline/NLP-preprocessing-pipeline.sh
-./automatic-summarization-pipeline/automatic-summarization-pipeline.sh
+cd NLP-preprocessing-pipeline
+./NLP-preprocessing-pipeline.sh
+cd ..
+cd automatic-summarization-pipeline
+./automatic-summarization-pipeline.sh
+cd ..
+
 
